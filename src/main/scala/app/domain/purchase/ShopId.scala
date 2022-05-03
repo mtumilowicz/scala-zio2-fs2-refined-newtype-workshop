2b018825dd7abda0a6f37eceeba1877479bc3b21
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-05-03 19:14:38 +0200

    newtype ShopId

--- src/main/scala/app/domain/purchase/ShopId.scala
+++ src/main/scala/app/domain/purchase/ShopId.scala
@@ -4,15 +4,3 @@ import app.domain.common.StringUtils._
 import cats.data._
 import cats.implicits._
 
-case class ShopId private(raw: String)
-
-object ShopId {
-  def apply(shopId: String): ValidatedNec[String, ShopId] =
-    if (condition(shopId))
-      new ShopId(shopId).validNec
-    else
-      s"ShopId: should start with a letter and contain only digits or letters!".invalidNec
-
-  private def condition(shopId: String): Boolean =
-    shopId.startsWithLetter() && shopId.lettersOrDigits()
-}
